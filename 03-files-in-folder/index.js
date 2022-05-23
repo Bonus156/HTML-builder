e8b9cb22e17a0c9ec2d33aa726f1363ed75e0ba9
Churyla Uladzimir <Bonus.by@mail.ru>
2022-05-23 18:45:28 +0300

    feat: done 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    if (file.isFile()) {
+      fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, data) => {
+        if (err) throw err;
+        console.log(`${file.name} - ${path.extname(file.name).slice(1)} - ${data.size / 1024} kb`);
+      });
+    }
+  });
+});
