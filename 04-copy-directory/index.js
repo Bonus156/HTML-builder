09a4fc823cc6f9f7866934145090b246a4f0a24d
Bonus156 <Bonus.by@mail.ru>
2022-11-02 00:04:48 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const path = require('path');
+const  fs = require('fs');
 
+const filesFolder = path.join(__dirname, 'files');
+const destFolder = path.join(__dirname, 'files-copy');
+
+function createFolder() {    
+  fs.mkdir(destFolder, { recursive: true }, (err) => {
+    if (err) throw err;
+  });  
+  fs.readdir(destFolder, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) console.log(err);
+      })
+    })
+  })  
+}
+
+
+function copyFiles() {
+  createFolder();
+  fs.readdir(filesFolder, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.copyFile(`${filesFolder}/${file}`, `${destFolder}/${file}`, (err) => {
+        if (err) console.log(err);
+      });
+    });
+  })
+}
+
+copyFiles();
